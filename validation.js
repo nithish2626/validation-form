7849cef4dbb1bdf02b18dfa7c5d96f65d2c425f9
nithish2626 <nithishnk9500@gmail.com>
2024-09-02 16:05:33 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,6 @@
+document.getElementById('visaApplicationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Perform validation or any other logic here
+    alert('Application Submitted Successfully!');
+});
